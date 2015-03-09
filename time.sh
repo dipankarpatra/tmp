0970952f5944b620acb164550ef38640f3efc147
dipankarpatra <dipankarpatra100@gmail.com>
2015-03-09 11:45:48 +0530

    Added Time Script

---
+++ time.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#-----------------------------------------------
+# Written for simple calculation of time taken
+# Dipankar <dipankarpatra100@gmail.com>
+#-----------------------------------------------
+
